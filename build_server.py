4f0db315d303bef9bbbab3946b318735842acd98
ZQPei <dfzspzq@163.com>
2019-12-15 11:23:42 +0800

    build_server

--- build_server.py
+++ build_server.py
@@ -1,9 +1,10 @@
 #!/home/pzq/anaconda3/bin/python
 
+import sys
 from huster.build_server import build_server, parse_args
 
 
 if __name__ == "__main__":
     args = parse_args()
     build_server(port=args.port, base_dir=args.base_dir)
-    
+    sys.exit()
